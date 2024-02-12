f4f93f99d544d17b8c338aa2d8bd8ca9cf65be68
Abdullah Asghar <61183273+AbdullahAsghar@users.noreply.github.com>
2024-02-12 08:42:52 +0500

    Create Question 5.sql

---
+++ Braintree_SQL_Coding_Challenge/SQL Scripts/Question 5.sql
@@ -0,0 +1,45 @@
+/*Find the sum of gpd_per_capita by year and the count of countries for each year that have non-null gdp_per_capita where (i) the year is before 2012 and (ii) the country has a null gdp_per_capita in 2012. Your result should have the columns:
+year
+country_count
+total
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Answer:
+1. Pretty basicquery structure, with a sub-query created first, named 'b', to extract all those records from per_capita table where year is 2012 and GDP per capita values are null.
+2. Once the temporary table is established, it is connected with the main query thorugh RIGHT JOIN to extract the sum of GDP per capita of all of these countries in all years before 2012 and their count in each year. */
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT 
+    a.year, 
+    COUNT(DISTINCT a.country_code) AS country_count, 
+    CONCAT('$', ROUND(SUM(a.gdp_per_capita), 2)) AS total
+FROM 
+    per_capita AS a
+RIGHT JOIN (
+    SELECT * FROM per_capita
+    WHERE year = 2012 AND gdp_per_capita IS NULL
+) AS b ON a.country_code = b.country_code
+WHERE 
+    a.year < 2012 AND a.gdp_per_capita IS NOT NULL
+GROUP BY 
+    a.year;
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*Solution:
+
+| year | country_count   | total               |
+|------|-----------------|---------------------|
+| 2004 | 15              | $491,203            |
+| 2005 | 15              | $510,735            |
+| 2006 | 14              | $553,690            |
+| 2007 | 14              | $654,509            |
+| 2008 | 10              | $574,016            |
+| 2009 | 9               | $473,103            |
+| 2010 | 4               | $179,751            |
+| 2011 | 4               | $199,153            |
+
+*/
+
