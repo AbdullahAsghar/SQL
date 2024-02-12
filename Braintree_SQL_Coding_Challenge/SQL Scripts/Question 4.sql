74f3521c6a982fb4fde81783b4837a8bc7ba5c9d
Abdullah Asghar <61183273+AbdullahAsghar@users.noreply.github.com>
2024-02-12 16:58:16 +0500

    Update Question 4.sql

--- Braintree_SQL_Coding_Challenge/SQL Scripts/Question 4.sql
+++ Braintree_SQL_Coding_Challenge/SQL Scripts/Question 4.sql
@@ -1,4 +1,4 @@
-/*Find the sum of gpd_per_capita by year and the count of countries for each year that have non-null gdp_per_capita where (i) the year is before 2012 and (ii) the country has a null gdp_per_capita in 2012. Your result should have the columns:
+/*4.Find the sum of gpd_per_capita by year and the count of countries for each year that have non-null gdp_per_capita where (i) the year is before 2012 and (ii) the country has a null gdp_per_capita in 2012. Your result should have the columns:
 year
 country_count
 total
